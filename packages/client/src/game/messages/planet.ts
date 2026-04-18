f7460cbb04d06b96704a26e24e2ecad485146b24
Paul Abrams <1376688+pabrams@users.noreply.github.com>
2026-04-18 19:16:41 -0400

    style

--- packages/client/src/game/messages/planet.ts
+++ packages/client/src/game/messages/planet.ts
@@ -6,14 +6,14 @@ import { makeDomain } from './_domain.js';
 
 export const PLANET = makeDomain('PLANET', {
     landing: '[bg]Landing on[/bg] [bc]{name}[/bc][by]...[/by]',
-    colonists: '  [by]Colonists[/by]: [w]{count}[/w]',
+    colonists: '  [by]Colonists[/by]: [bg]{count}[/bg]',
 
     prompt: '\r\n[mg]Planet command[/mg] [mg]([/mg][by]?[/by]=[by]Help[/by][mg])[/mg] [by]?[/by] ',
 
     earthHeader: '[bg]Earth[/bg] — [by]Colonists[/by]: [w]{count}[/w]',
 
-    takePrompt: '\r\n[c]How many colonists to take?[/c] ',
-    leavePrompt: '\r\n[c]How many colonists to leave?[/c] ',
+    takePrompt: '\r\n[by]How many colonists to take?[/by] ',
+    leavePrompt: '\r\n[by]How many colonists to leave?[/by] ',
 
     takeCommodityHeader: '[bc]Which colonists to take?[/bc]',
     leaveCommodityHeader: '[bc]Assign colonists to which commodity?[/bc]',
