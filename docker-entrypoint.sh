b703f385345e54080dc92f84ce33922f034c73fe
Paul Abrams <1376688+pabrams@users.noreply.github.com>
2026-04-27 21:31:23 -0400

    add reset-db switch to deploy

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 set -e
 
-# Manual one-shot: SEED_UNIVERSE=1 generates and imports universe data.
-if [ "$SEED_UNIVERSE" = "1" ]; then
-  echo "Seeding universe..."
-  node packages/server/scripts/twnr-bigbang.js packages/server/data/universe/ --sectors 500 --seed 42
-  node packages/server/scripts/importUniverse.js packages/server/data/universe --force
-  echo "Seed complete."
-fi
 
 exec "$@"
