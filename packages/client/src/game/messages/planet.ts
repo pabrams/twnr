cac2cbfdca6c917c171c5dc794bf45ae9e9086e5
Paul Abrams <1376688+pabrams@users.noreply.github.com>
2026-05-03 15:43:25 -0400

    messaging(planet earth menu)

--- packages/client/src/game/messages/planet.ts
+++ packages/client/src/game/messages/planet.ts
@@ -8,12 +8,13 @@ export const PLANET = makeDomain('PLANET', {
     landing: '[bg]Landing on[/bg] [bc]{name}[/bc][by]...[/by]',
     colonists: '  [by]Colonists[/by]: [bg]{count}[/bg]',
 
-    prompt: '\r\n[mg]Planet command[/mg] [mg]([/mg][by]?[/by]=[by]Help[/by][mg])[/mg] [by]?[/by] ',
+    prompt: '\r\n[mg]Planet command[/mg] [mg]([/mg][by]?[/by]=[by]Help[/by][mg])[/mg] [by]:[/by] ',
 
-    earthHeader: '[bg]Earth[/bg] — [by]Colonists[/by]: [w]{count}[/w]',
+    earthHeader: '[mg]There are [/bg] [by]{count}[/by] colonists waiting on earth to colonize distant planets.\r\n' +
+                 'You can repurpose your ship\'s storage to house 1 colonist per cargo hold.',
 
     takePrompt:
-        '\r\n[by]How many colonists to take?[/by] [mg]([/mg][bc]{emptyHolds}[/bc] [g]free holds[/g][mg])[/mg] [mg][[/mg][bc]{emptyHolds}[/bc][mg]][/mg] ',
+        '\r\n[mg]How many colonists to take?[/mg] ([/mg][bc]{emptyHolds}[/bc] [g]free holds[/g][mg]) [[/mg][by]{emptyHolds}[/by][mg]][/mg] [by]:[/by] ',
     leavePrompt:
         '\r\n[by]How many colonists to leave?[/by] [mg]([/mg][bc]{shipColonists}[/bc] [g]on board[/g][mg])[/mg] [mg][[/mg][bc]{shipColonists}[/bc][mg]][/mg] ',
 
